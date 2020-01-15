d85edee60929bfcfe0246104eb5b272865b0e4ae
lijian <150443668461@163.com>
2020-01-15 13:49:53 +0800

    hash

--- src/hash/Main705.java
+++ src/hash/Main705.java
@@ -8,7 +8,23 @@ import java.util.HashSet;
  * @date 2020/1/14
  */
 public class Main705 {
-    public static void main(String[] args) {
+    boolean[] set = new boolean[1000001];
+    /** Initialize your data structure here. */
+    public Main705() {
 
     }
+
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return set[key];
+    }
 }
+
